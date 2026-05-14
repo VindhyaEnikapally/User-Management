9cc84ffae33c565ca7c1af6808e4158c9ed1624e
VindhyaEnikapally <23eg105e15@anurag.edu.in>
2026-05-14 11:50:36 +0530

    final code

--- frontend/src/Components/RootLayout.jsx
+++ frontend/src/Components/RootLayout.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header.jsx";
-import Footer from "./Footer.jsx";
 
 export default function RootLayout() {
   return (
