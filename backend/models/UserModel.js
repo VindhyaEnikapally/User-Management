496e6fa77684534d64f7d1b575ddfeb1dba10469
VindhyaEnikapally <23eg105e15@anurag.edu.in>
2026-05-14 20:24:36 +0530

    Completed user management app

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,34 +1,38 @@
-import {Schema,model} from 'mongoose';
+import { Schema, model } from 'mongoose';
 
 //Create user Schema with validations
-const userSchema=new Schema({
- username:{
-  type:String,
-  required:[true,"Name is required"]
- },
- email:{
-  type:String,
-  required:[true,"Email is required"],
-  unique:[true,"Email already exists"]
- },
- dateOfBirth:{
- type:Date,
-  required:[true,"DOB is required"]
- },
- mobileNumber:{
-  type:Number
- },
- //for soft delete 
- status:{
-    type:Boolean,
-    default:true
- }
+const userSchema = new Schema({
 
-},{
- timestamps:true,
- versionKey:false,
- strict:"throw"
+  username: {
+    type: String,
+    required: [true, "Name is required"]
+  },
+
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true
+  },
+
+  dateOfBirth: {
+    type: Date,
+    required: [true, "DOB is required"]
+  },
+
+  mobileNumber: {
+    type: Number
+  },
+
+  //for soft delete
+  status: {
+    type: Boolean,
+    default: true
+  }
+
+}, {
+  timestamps: true,
+  versionKey: false
 });
 
-//Create User Model for User Schema
-export const UserModel=model('user',userSchema);
+//Create User Model
+export const UserModel = model('user', userSchema);
